8e91cef6b59a35b5089befaddf4977889ff7a2c9
Mateusz Wijas <mmw@users.noreply.github.com>
2016-08-02 10:10:43 -0400

    Min length support (#4)
    * ignore log files
    
    * start script
    
    * add validity support based on minLength

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 import React from 'react';
+import classnames from 'classnames';
 
 export default class ReactPasswordStrength extends React.Component {
   constructor(props) {
@@ -8,28 +9,43 @@ export default class ReactPasswordStrength extends React.Component {
 
     this.state = {
       score: 0,
+      isValid: false,
+      password: '',
     };
   }
 
   handleChange(e) {
-    const score = this.refs["ReactPasswordStrength-input"].value.length;
+    const password = this.refs['ReactPasswordStrength-input'].value;
+    const score = password.length;
+    const changeCallback = this.props.changeCallback;
 
     this.setState({
+      isValid: score > this.props.minLength,
+      password,
       score: score > 4 ? 4 : score,
     });
+
+    if (changeCallback !== null) {
+      changeCallback(this.state);
+    }
   }
 
   render() {
-    const { score } = this.state;
-    const { scoreWords, inputProps } = this.props;
+    const { score, password, isValid } = this.state;
+    const { scoreWords, inputProps, minLength } = this.props;
+    const inputClasses = classnames('ReactPasswordStrength-input', {
+      'is-password-valid': isValid,
+      'is-password-invalid': isValid === false && password.length > 0,
+    });
 
     return (
       <div className="ReactPasswordStrength">
         <input
-          className="ReactPasswordStrength-input"
+          className={inputClasses}
           onChange={this.handleChange.bind(this)}
           ref="ReactPasswordStrength-input"
           type="password"
+          value={password}
           {...inputProps}
         />
 
@@ -42,10 +58,14 @@ export default class ReactPasswordStrength extends React.Component {
 }
 
 ReactPasswordStrength.propTypes = {
+  changeCallback: React.PropTypes.func,
   inputProps: React.PropTypes.object,
+  minLength: React.PropTypes.number,
   scoreWords: React.PropTypes.array,
 };
 
 ReactPasswordStrength.defaultProps = {
+  changeCallback: null,
+  minLength: 5,
   scoreWords: ['Weak', 'Weak', 'Okay', 'Good', 'Strong'],
 };
