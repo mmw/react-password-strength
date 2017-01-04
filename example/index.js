07746cb627e6aa15556efe06786390f648f946c7
Mateusz Wijas <mateusz.wijas+github@gmail.com>
2017-01-04 10:20:37 -0500

    update example

--- example/index.js
+++ example/index.js
@@ -2,17 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactPasswordStrength from "../dist/index";
 
-const inputProps = {
-  placeholder: "Try a password...",
-  id: "inputPassword",
-  autoFocus: true,
-};
+class App extends React.Component {
+  constructor() {
+    super();
 
-ReactDOM.render(
-  <div>
-    <h1>React Password Strength Tool</h1>
-    <p>Powered by <a href="https://github.com/dropbox/zxcvbn" target="_blank">zxcvbn</a></p>
-    <ReactPasswordStrength inputProps={inputProps} />
-  </div>,
-  document.getElementById("example")
-);
+    this.state = {
+      passLength: 0,
+    };
+  }
+
+  changeCallback(state) {
+    this.setState({ passLength: state.password.length });
+  }
+
+  clear() {
+    this.refs.passComponent.clear();
+  }
+
+  render() {
+    const inputProps = {
+      placeholder: "Try a password...",
+      id: "inputPassword",
+      autoFocus: true,
+    };
+
+    return (
+      <div>
+        <h1>React Password Strength Tool</h1>
+        <p>Powered by <a href="https://github.com/dropbox/zxcvbn" target="_blank">zxcvbn</a></p>
+        <ReactPasswordStrength
+          ref="passComponent"
+          inputProps={inputProps}
+          changeCallback={this.changeCallback.bind(this)}
+        />
+        <button
+          onClick={this.clear.bind(this)}
+          disabled={this.state.passLength === 0}
+        >
+          Clear
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("example"));
